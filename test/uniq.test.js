8fe37003a09fdb51ca6db2b0e9e0fa9ee0ba999a
azmeer <azmeer.salleh@gmail.com>
2017-05-08 16:58:42 -0700

    Adding unit test for uniq

--- test/uniq.test.js
+++ test/uniq.test.js
@@ -5,4 +5,30 @@ describe('uniq()', () => {
     const nums = [2, 4, 5, 5, 7, 4, 10, 2];
     expect(_.uniq(nums)).toEqual([2, 4, 5, 7, 10]);
   });
+
+  it('de-dups the values of an object', () => {
+    const fruit = { 1: 'orange',
+                    2: 'kumquat',
+                    3: 'apple',
+                    4: 'cherry',
+                    5: 'orange'
+                  };
+    expect(_.uniq(fruit)).toEqual(['orange', 'kumquat', 'apple', 'cherry']);
+  });
+  
+  it('de-dups an array-like object', () => {
+    const assorted = {
+      length: 6,
+      0: 'a',
+      1: 3,
+      2: 2342,
+      3: 'Hello, Mrs. Robinson',
+      4: 'Jeremy',
+      5: 2342,
+      sancho: 'panza',
+      theonetrueanswer: 42
+    };
+    expect(_.uniq(assorted)).
+      toEqual(['a', 3, 2342, 'Hello, Mrs. Robinson', 'Jeremy']);
+  });
 });
