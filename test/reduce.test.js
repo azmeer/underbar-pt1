2f2713676b11042d45564328aa10bd3895ff2c65
azmeer <azmeer.salleh@gmail.com>
2017-05-08 15:46:40 -0700

    Adding unit test for reduce

--- test/reduce.test.js
+++ test/reduce.test.js
@@ -13,4 +13,14 @@ describe('reduce()', () => {
     expect(result).toEqual(15);
   });
 
+  it('reduces an object containing people to a single string of their ages', () => {
+     const people = {
+       'Harriet' : {age: 12},
+       'Lazarus' : {age: 999},
+       'Bethany' : {age: 14}
+     };
+    const result = _.reduce(people, (ages, person) =>
+                            ages + ((ages.length > 0) ? ', ': '') + person.age, "");
+     expect(result).toEqual('12, 999, 14');
+  });
 });
