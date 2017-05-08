766f674edf47d87f1d5c38044c2f1e861a462505
azmeer <azmeer.salleh@gmail.com>
2017-05-08 15:21:45 -0700

    Adding unit tests for pluck

--- test/pluck.test.js
+++ test/pluck.test.js
@@ -11,4 +11,13 @@ describe('pluck()', () => {
     expect(result).toEqual([12, 999, 14]);
   });
 
+  it('it returns an array of ages, given an object with people properties', () => {
+    const people = {
+      'Harriet': {age: 12},
+      'Lazarus': {age: 999},
+      'Bethany': {age: 14}
+    };
+    const result = _.pluck(people, 'age');
+    expect(result).toEqual([12, 999, 14]);
+  });
 });
