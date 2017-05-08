c4344f79a0223691f8a6d51d6b5c4a8440f55f77
azmeer <azmeer.salleh@gmail.com>
2017-05-08 16:44:43 -0700

    Adding unit tests for some

--- test/some.test.js
+++ test/some.test.js
@@ -11,4 +11,34 @@ describe('some()', () => {
     expect(_.some(nums, num => num % 2 === 1)).toBe(false);
   });
 
+  it('returns true if any number is odd in an object', () => {
+    const nums = {a: 2, b: 4, c: 5, d: 6};
+    expect(_.some(nums, num => num % 2 === 1)).toBe(true);
+  });
+
+  it('returns false if no number is odd in an object', () => {
+    const nums = {a: 2, b: 4, c: 6, d: 8};
+    expect(_.some(nums, num => num % 2 === 1)).toBe(false);
+  });
+
+  
+  it('returns true if at least one of the indexed numbers in an array-like object test positive', () => {
+    const arrayLikeObj = { length: 4,
+                           0: 5,
+                           1: 2,
+                           2: 6,
+                           3: 15
+                         };
+    expect(_.some(arrayLikeObj, num => num > 10)).toBe(true);
+  });
+
+  it('returns false if none of the indexed numbers in an array-like object test positive', () => {
+    const arrayLikeObj = { length: 3,
+                           0: 5,
+                           1: 2,
+                           2: 6,
+                           3: 15
+                         };
+    expect(_.some(arrayLikeObj, num => num > 10)).toBe(false);
+  });
 });
